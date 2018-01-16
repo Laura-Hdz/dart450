48d9ec5e1520016b8f8abda17f9bdc511fae9483
Laura-Hdz <laurah@MacBook-Pro.local>
2018-01-16 16:07:59 -0500

    exercises update

---
+++ exercises/exercise02/js/script.js
@@ -0,0 +1,15 @@
+/**********************************************
+
+DART 450, Winter 2018
+Exercise 02 (Jan 16)
+Laura Hernandez
+
+js for the second exercise of the semester.
+
+**********************************************/
+
+$(document).ready(function () {
+
+  // Insert jQuery code here to run when the page is loaded
+
+});

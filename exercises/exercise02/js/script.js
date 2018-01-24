7f9647bc8a4deb349bed76a2d45465777ea4df16
Laura-Hdz <laurah@MacBook-Pro.local>
2018-01-23 20:07:54 -0500

    Week 3 & Exercise 02

--- exercises/exercise02/js/script.js
+++ exercises/exercise02/js/script.js
@@ -11,5 +11,24 @@ js for the second exercise of the semester.
 $(document).ready(function () {
 
   // Insert jQuery code here to run when the page is loaded
+  $('#esports').on('click',function () {
+  $('#wrapper').animate({backgroundColor:'#450881'},500);
+  $('#esports').fadeToggle();
+  $('#quote').fadeIn();
+  });
+
+  $('.click').on('mouseover',function () {
+  $('#source').fadeIn();
+  });
+
+  $('#source').on('click', function () {
+  $('#whyContainer').addClass("whyClass", 500, "easeInBack").toggle("drop");
+  $('#why').delay(1000).fadeToggle();
+  $('#reset').delay(1000).fadeToggle();
+  });
+
+  $('#reset').on('click', function () {
+    location.reload();
+  });
 
 });

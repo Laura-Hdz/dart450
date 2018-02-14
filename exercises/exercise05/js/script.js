6f4f6a80b1de7be3c624a00e6fa37b726ea81963
Laura-Hdz <laurah@MacBook-Pro.local>
2018-02-13 23:38:42 -0500

    ex5

--- exercises/exercise05/js/script.js
+++ exercises/exercise05/js/script.js
@@ -7,9 +7,84 @@ Author
 Description of what the script does...
 
 **********************************************/
+var firstAnswer = 2+3+6+7+234+90-57+567+3+3245+66+192-8+1+1+1+4-123+567+7+4-786+0+0+6+2+3+6+7+234+90-57+567+3+3245+66+192-8+1+1+1+4-123+567+7+4-786+0;
+var secondAnswer = 2343+90-57+567+3+3245+66+192-8+1+1+1+4-123+2+39+61+73+2374+905-571+5678+34+32457+661+1925-86+19+13+15+46-1232+5677+79+41-7860+20+50+76+29+323+66+78+567+7+4-786+0;
+var thirdAnswer = 4-123+567+7+4-786+0+0+6+2+3+6+7+234+90-57+34+32457+661+1925-86+19+13+15+46-1232+5677+79+234+90-57+567+4-786+0+0+6+3+3245+66+192-8+1+1+1+4-123+567;
+var fourthAnswer = 567+7+4-786+0+0+6+2+3+6+7+234+90-57+567-2+3+6+7+234+90-57+567+3+3245+66+192-8+1+1+1+4-123+567+7+4-786+0+0+6+2+3+6+7+234+90-57+567;
+var fifthAnswer = 90-57+567+3+3245+66+192-8+1+1+1+4-123+567+7+4-786+0+0+6+2+3+6+7+234+90-57+567+3+3245+66+192-8+1+1+1+4-123+567+7+4-786+1+57+32+2+3+6;
+var sixthAnswer = 1+4-123+567+7+4-786+0+0+6+2+3+6+7+234+90-57+567+3+3245+66+192-8+1+1+1+4-123+567+7+4-786+0+0+62+3+6+7+234+90-57+567+3+3245+66+192-8+1;
 
 $(document).ready(function () {
 
-  // Insert jQuery code here to run when the page is loaded
+
+  $('input').click(function () {
+  var bottom = Math.random() * 600;
+  var left = Math.random() * 600;
+  $(this).css({
+      position: 'absolute',
+      bottom: bottom,
+      left: left,
+      fontSize: '4px',
+      color: 'yellow'
+      });
+  });
+
+  $('#correct').hide();
+  $('#wrong').hide();
+
+  $('#enterQ1').on('click',function () {
+    if (inputQ1 === firstAnswer) {
+    $('#correct').show();
+    }
+    else {
+    $('#wrong').show();
+    }
+ });
+
+   $('#enterQ2').on('click',function () {
+     if (inputQ2 === secondAnswer) {
+      $('#correct').show();
+     }
+     else {
+       $('#wrong').show();
+     }
+  });
+
+  $('#enterQ3').on('click',function () {
+    if (inputQ3 === thirdAnswer) {
+     $('#correct').show();
+    }
+    else {
+      $('#wrong').show();
+    }
+  });
+
+  $('#enterQ4').on('click',function () {
+    if (inputQ4 === fourthAnswer) {
+     $('#correct').show();
+    }
+    else {
+      $('#wrong').show();
+    }
+  });
+
+  $('#enterQ5').on('click',function () {
+    if (inputQ5 === fifthAnswer) {
+     $('#correct').show();
+    }
+    else {
+      $('#wrong').show();
+    }
+  });
+
+  $('#enterQ6').on('click',function () {
+    if (inputQ6 === sixthAnswer) {
+     $('#correct').show();
+    }
+    else {
+      $('#wrong').show();
+    }
+  });
+
 
 });
---
+++ week06/js/script.js
@@ -0,0 +1,108 @@
+/**********************************************
+
+DART 450, Winter 2018
+week 06 Lecture
+
+Functions, style, debugging, plugins
+
+**********************************************/
+
+// $(document).ready(function () {
+
+  // $(document).ready(function () {
+  //   $(document).on('click',function () {
+  //     var $square = $('<div></div>');
+  //     var x = Math.floor(Math.random() * $(window).width());
+  //     var y = Math.floor(Math.random() * $(window).height());
+  //     $square.css({
+  //       position: 'absolute',
+  //       top: y + 'px',
+  //       left: x + 'px',
+  //       width: '100px',
+  //       height: '100px',
+  //       backgroundColor: 'red'
+  //     });
+  //     $('body').append($square);
+  //   });
+  // });
+
+  // $(document).ready(function () {
+  //   $(document).on('click',function () {
+  //     addRedSquare();
+  //   });
+  // });
+  //
+  // function addRedSquare() {
+  //   var $square = $('<div></div>');
+  //   var x = Math.floor(Math.random() * $(window).width());
+  //   var y = Math.floor(Math.random() * $(window).height());
+  //   $square.css({
+  //     position: 'absolute',
+  //     top: y + 'px',
+  //     left: x + 'px',
+  //     width: '100px',
+  //     height: '100px',
+  //     backgroundColor: 'red'
+  //   });
+  //   $('body').append($square);
+  // }
+  // });
+
+  /*
+
+  Basic Blasting
+  Pippin Barr
+
+  Some examples of the basics of the Blast plugin. Essentially changing the
+  styling of characters/words/sentences.
+
+  */
+
+  $(document).ready(function() {
+
+    // Most basically, we can just call .blast() on an element
+    // to divide it into spans (around words in this case) each with
+    // a class of 'blast'
+    $('#basicblast').blast();
+
+    // More creatively, though, we can call .blast() and then listen for an event
+    // on each of the elements created by blast (e.g. in this case
+    // each span around each character)
+    $('#blastbycharacter').blast({
+      delimiter: 'character'
+    }).mouseover(fadeToBlack);
+
+    // Same thing for each word
+    $('#blastbyword').blast({
+      delimiter: 'word'
+    }).mouseover(fadeToBlack);
+
+    // Same thing for each sentence
+    $('#blastbysentence').blast({
+      delimiter: 'sentence'
+    }).mouseover(fadeToBlack);
+
+    // We can also use blast to select and put spans around specific
+    // search text (like 'nec') and also to add a custom class to the
+    // things it finds, and even a specific numbered id...
+    $('#blastbysearch').blast({
+      search: 'sex',
+      customClass: 'nec',
+      generateIndexID: true
+    }).mouseover(fadeToBlack);
+
+  });
+
+
+  // fadeToBlack (index)
+  //
+  // A simple function to fade the background colour of the element
+  // to black
+  function fadeToBlack () {
+    // We'll use jQuery's animate function here
+    // Note that because we're animating a colour, we have to include jQuery UI
+    // in our index.html
+    $(this).animate({
+        backgroundColor: 'black',
+    },1000);
+  }

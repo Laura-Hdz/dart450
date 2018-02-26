764b628688ab650b27d8c5072739e3752c1f4fa7
Laura-Hdz <laurah@MacBook-Pro.local>
2018-02-25 22:42:52 -0500

    Midterm - Blobs Part 1
    Part 1 of the Blob conversation (HTML, CSS and js)

--- midterm/js/script.js
+++ midterm/js/script.js
@@ -11,7 +11,67 @@ Online communication has become a big part of what the Internet is. And what bet
 **********************************************/
 
 $(document).ready(function () {
+// Insert jQuery code here to run when the page is loaded
 
-  // Insert jQuery code here to run when the page is loaded
+  //Welcome window alert popup when page loads
+  // window.alert("Welcome to Blob chat! Enjoy your stay and Blob away!")
+
+  //Start the chat by hiding the text by clicking on it and sending in the three blobs to start the convo
+  $('#startChatting').on('click',function () {
+    $('#startChatting').fadeToggle();
+    $('#hiBlob').fadeIn().css({
+      top: '50%'
+    });
+    $('#sadBlob').fadeIn().css({
+      top: '50%'
+    });
+    $('#worriedBlob').fadeIn().css({
+      top: '50%'
+    });
+  });
+
+  //Find out what the Blobs mean by hovering over them and appending their respective text
+
+  $('#hiBlob').on('mouseover',function () {
+    $('#hiBlob').append('Hi! ');
+  });
+
+  $('#sadBlob').on('mouseover',function () {
+    $('#sadBlob').append('WHY DOES THIS HAPPEN TO MEEEE ');
+  });
+
+  $('#worriedBlob').on('mouseover',function () {
+    $('#worriedBlob').append('oh god ');
+  });
+
+  //Decide where the conversation goes depending on which blob you click on
+  $('#hiBlob').on('click', function () {
+    document.getElementById('mainBlob').src='images/ablobgrin.gif';
+    $('#hiBlob').fadeToggle();
+    $('#sadBlob').fadeToggle();
+    $('#worriedBlob').fadeToggle();
+      setInterval(function () { 
+        $('#giftBlob').fadeIn();
+        $('#winkBlob').fadeIn();
+       },1000);
+  });
+
+  $('#sadBlob').on('click', function () {
+    document.getElementById('mainBlob').src='images/ablobnervous.gif';
+    $('#hiBlob').fadeToggle();
+    $('#sadBlob').fadeToggle();
+    $('#worriedBlob').fadeToggle();
+      setInterval(function () { 
+        $('#sadCloudBlob').fadeIn();
+        $('#dizzyBlob').fadeIn();
+       },1000);
+  });
+
+  $('#worriedBlob').on('click', function () {
+    document.getElementById('mainBlob').src='images/ablobnervous.gif';
+    $('#hiBlob').fadeToggle();
+    $('#sadBlob').fadeToggle();
+    $('#worriedBlob').fadeToggle();
+  });
 
 });

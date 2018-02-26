15dec599c8bfea22bf91cd15de88235dfc884c1c
Laura-Hdz <laurah@MacBook-Pro.local>
2018-02-25 19:38:06 -0500

    Midterm - Creating Folders
    css, js, images + index

---
+++ midterm/js/script.js
@@ -0,0 +1,17 @@
+/**********************************************
+
+DART 450, Winter 2018
+Midterm / The Blobs
+Laura Hernandez
+
+Online communication has become a big part of what the Internet is. And what better way to communicate than through Blobs!
+
+
+
+**********************************************/
+
+$(document).ready(function () {
+
+  // Insert jQuery code here to run when the page is loaded
+
+});

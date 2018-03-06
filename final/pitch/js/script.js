f84315fd1dfde1c9b2e896b8032e094af6d9d171
Laura-Hdz <laurah@MacBook-Pro.local>
2018-03-06 13:31:42 -0500

    Final - Pitch: Fixing js/css/html and adding README

--- final/pitch/js/script.js
+++ final/pitch/js/script.js
@@ -18,25 +18,29 @@ $(document).ready(function() {
       'no': noLunch,
       'I dont want to talk about it': sadBob,
       };
-
     annyang.addCommands(commands);
-
     annyang.start();
+    }
 
-  }
-
-});
+// });
 
 function ateLunch () {
-  $('#status').text('Woo! Ate a great lunch today.')
+  $('#status').text('Woo! Ate a great lunch today.');
+  $('#no').hide();
+  $('#yes').show();
 }
 
 function noLunch () {
   $('#status').text('Man, starving right now.')
+  $('#yes').hide();
+  $('#no').show();
 }
 
 function sadBob () {
   $('#status').text('Feeling down right now...')
+  $('#yes').hide();
+  $('#no').hide();
+  $('#talktome').show();
 }
 
 });
